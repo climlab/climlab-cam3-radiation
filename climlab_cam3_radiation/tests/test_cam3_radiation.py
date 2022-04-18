dc9f3f8a091fbe7c8ebbe6bbe201201594cdff85
Brian Rose <brose@albany.edu>
2022-04-18 15:27:20 -0400

    Use pooch and xarray to auto-initialize on import

--- climlab_cam3_radiation/tests/test_cam3_radiation.py
+++ climlab_cam3_radiation/tests/test_cam3_radiation.py
@@ -4,5 +4,17 @@ import climlab_cam3_radiation
 from climlab_cam3_radiation import driver, absems
 
 
+
 def test_cam3_radiation():
     pass
+    # # Call the CAM3 radiation driver
+    # (TdotRad, SrfRadFlx, qrs, qrl, swflx, swflxc, lwflx, lwflxc, SwToaCf,
+    #  SwSrfCf, LwToaCf, LwSrfCf, LwToa, LwSrf, SwToa, SwSrf,
+    #  swuflx, swdflx, swuflxc, swdflxc,
+    #  lwuflx, lwdflx, lwuflxc, lwdflxc) = \
+    #     driver(KM, JM, IM, do_sw, do_lw, p, dp, ps, Tatm, Ts,
+    #            q, O3mmr, cldfrac, clwp, ciwp, in_cld,
+    #            aldif, aldir, asdif, asdir, eccf, coszen,
+    #            scon, flus, r_liq, r_ice,
+    #            CO2vmr, N2Ovmr, CH4vmr, CFC11vmr,
+    #            CFC12vmr, g, Cpd, epsilon, stebol)
